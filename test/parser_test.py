2d3a942609faecae119253d01670256276b101ec
birkin <birkin.diana@gmail.com>
2017-06-30 13:05:10 -0400

    improves open usage.

--- test/parser_test.py
+++ test/parser_test.py
@@ -17,7 +17,8 @@ class ParserTest(unittest.TestCase):
 
     def test_login(self):
         path = os.path.join(DATA_PATH, 'login.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
         login = parsers.main_menu(content)
         self.assertTrue(login['authenticated'])
         self.assertEqual(login['session_id'],
@@ -25,7 +26,8 @@ class ParserTest(unittest.TestCase):
 
     def test_needs_registration(self):
         path = os.path.join(DATA_PATH, 'not_registered.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
         login = parsers.main_menu(content)
         self.assertFalse(login['registered'])
 
@@ -37,9 +39,10 @@ class ParserTest(unittest.TestCase):
             for k in key_list:
                 self.assertTrue(k in rdict,
                                 msg="Request key missing key -- %s." % k)
-
         path = os.path.join(DATA_PATH, 'article_request.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
+        self.assertEqual( str, type(content) )  # content is unicode
         submit_details = parsers.request_form(content)
         request_key = submit_details
         _key_check(['PhotoJournalInclusivePages',
@@ -57,7 +60,8 @@ class ParserTest(unittest.TestCase):
 
     def test_blocked_patron(self):
         path = os.path.join(DATA_PATH, 'blocked.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
         request_key = parsers.request_form(content)
         self.assertTrue(request_key['blocked'])
         blocked_found = request_key['errors'].rfind('blocked')
@@ -65,7 +69,8 @@ class ParserTest(unittest.TestCase):
 
     def test_completed_request_submission(self):
         path = os.path.join(DATA_PATH, 'completed_request.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
         submit = parsers.request_submission(content)
         self.assertFalse(submit['error'])
         self.assertEqual(
@@ -76,7 +81,8 @@ class ParserTest(unittest.TestCase):
 
     def test_failed_request_submission(self):
         path = os.path.join(DATA_PATH, 'failed_request_missing_field.html')
-        content = open(path).read()
+        with open( path, 'rt' ) as f:
+            content = f.read()
         submit = parsers.request_submission(content)
         self.assertTrue(submit['error'])
         self.assertEqual(submit['message'],
