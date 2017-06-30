3c8bd09b840363c42212750f885da881019e075b
birkin <birkin.diana@gmail.com>
2017-06-30 14:16:30 -0400

    updates version.

--- sample_script.py
+++ sample_script.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
 import logging, os, pprint
 from illiad3.account import IlliadSession
 
--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
-setup(name='illiad',
-    version='0.8',
+setup(name='illiad3',
+    version='0.9',
     packages=find_packages(),
     install_requires=[
         'requests',
