d9dc3a1dbdc9b4c3181111eedc02867ab0d59088
birkin <birkin.diana@gmail.com>
2018-07-13 09:06:20 -0400

    bumps version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='illiad3',
-    version='0.11',
+    version='0.12',
     packages=find_packages(),
     install_requires=[
         'requests',
